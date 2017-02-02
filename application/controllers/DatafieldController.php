1d048b375c3ca3e97cd1209641338ca5ae680619
Marc DeTrano <marc@gridshield.net>
2017-02-02 16:10:53 -0600

    Implement GET API request for Datafield object

--- application/controllers/DatafieldController.php
+++ application/controllers/DatafieldController.php
@@ -4,9 +4,37 @@ namespace Icinga\Module\Director\Controllers;
 
 use Icinga\Module\Director\Forms\DirectorDatafieldForm;
 use Icinga\Module\Director\Web\Controller\ActionController;
+use Icinga\Module\Director\Objects\DirectorDatafield;
+use Icinga\Exception\NotFoundError;
+use Icinga\Exception\IcingaException;
+use Icinga\Module\Director\Objects\DirectorDatalist;
 
 class DatafieldController extends ActionController
 {
+    protected $isApified = true;
+    protected $object;
+
+    public function init() {
+        parent::init();
+        if ($this->getRequest()->isApiRequest()) {
+            $response = $this->getResponse();
+            try {
+                $this->loadObject();
+                return $this->handleApiRequest();
+            } catch (NotFoundError $e) {
+                $response->setHttpResponseCode(404);
+                return $this->sendJson($response, (object) array('error' => $e->getMessage()));
+            } catch (Exception $e) {
+                if ($response->getHttpResponseCode() === 200) {
+                    $response->setHttpResponseCode(500);
+                }
+
+                return $this->sendJson($response, (object) array('error' => $e->getMessage()));
+            }
+        }
+    }
+
+
     public function addAction()
     {
         $this->indexAction();
@@ -19,6 +47,10 @@ class DatafieldController extends ActionController
 
     public function indexAction()
     {
+        if ($this->getRequest()->isApiRequest()) {
+            return;
+        }
+
         $form = DirectorDatafieldForm::load()
             ->setDb($this->db());
 
@@ -37,4 +69,200 @@ class DatafieldController extends ActionController
         $form->handleRequest();
         $this->content()->add($form);
     }
+
+
+    protected function loadObject()
+    {
+        #Note: assuming varname is unique for API purposes, but this does seem to be enforced by the database
+        if (!$this->getRequest()->getParam('name')) {
+            return;
+        }
+        $query = $this->db()->getDbAdapter()
+            ->select()
+            ->from('director_datafield')
+            ->where('varname = ?', $this->getRequest()->getParam('name'));
+
+        $result = DirectorDatafield::loadAll($this->db(), $query);
+        if (!count($result)) {
+            throw new NotFoundError('Got invalid name "%s"', $this->getRequest()->getParam('name'));
+        }
+        $this->object=current($result);
+        return current($result);
+    }
+
+    protected function handleApiRequest() {
+        $request = $this->getRequest();
+        $response = $this->getResponse();
+        $db = $this->db();
+
+        switch ($request->getMethod()) {
+            case 'GET':
+                $this->requireObject();
+                $props=$this->restProps($this->object);
+                $this->sendJson($response,$props);
+                return;
+
+            case 'PUT':
+            case 'POST':
+                $data = json_decode($request->getRawBody());
+
+                if ($data === null) {
+                    $this->getResponse()->setHttpResponseCode(400);
+                    throw new IcingaException(
+                        'Invalid JSON: %s' . $request->getRawBody(),
+                        $this->getLastJsonError()
+                    );
+                } else {
+                    $data = (array) $data;
+                }
+                if (!empty($data['object_name'])) {
+                    $data['varname'] = $data['object_name'];
+                    unset($data['object_name']);
+                }
+
+                if (!empty($data['datalist_name'])) {
+                    $query = $this->db()->getDbAdapter()
+                        ->select()
+                        ->from('director_datalist')
+                       ->where('list_name = ?', $data['datalist_name']);
+
+                    $result = DirectorDatalist::loadAll($this->db(), $query);
+                    if (!count($result)) {
+                        $response->setHttpResponseCode(400);
+                        throw new IcingaException('Got invalid datalist_name: '.$data['datalist_name']);
+                    } else {
+                        $datalist = current($result);
+                    }
+                }
+                if (!empty($data['target_data_type'])) {
+                    $target_data_type=$data['target_data_type'];
+                }
+	
+                unset($data['object_type']);
+
+                $modified=false;
+
+                if ($object = $this->object) {
+                    $old_props = $this->restProps($object);
+                 
+                    if (isset($datalist)) {
+                        if (isset($old_props['datalist_name'])){
+                            $modified = $datalist->list_name != $old_props['datalist_name'];
+                        } else {
+                            $modified = true;
+                        }
+                    }
+                    foreach(array('target_data_type', 'icinga_object_type','query','resource') as $check_setting) {
+                        if (!empty($data[$check_setting])) {
+                            if (isset($old_props[$data[$check_setting]])){
+                                $modified = $data[$check_setting] != $old_props[$check_setting];
+                            } else {
+                                $modified = true;
+                            }
+                        }
+                    }
+                       
+                    if ($request->getMethod() === 'POST') {
+                        $object->setProperties($data);
+                    } else {
+                        $data = array_merge(array('varname' => $object->get('varname')),$data);
+                        $tmp = DirectorDatafield::create($data, $db);
+                        $tmp->setProperties($data);
+                        $replacement = array_merge($tmp->getProperties(),$data);
+                        unset($replacement['id']);
+                        $object->setProperties($replacement);
+                    }
+                } else {
+                    if (empty($data['varname'])) {
+                        $response->setHttpResponseCode(400);
+                        throw new IcingaException('Must specifiy object_name');
+                    }
+
+                    //The API will not allow duplicate varnames
+                    $newname=$data['varname'];
+                    $query = $this->db()->getDbAdapter()
+                        ->select()
+                        ->from('director_datafield')
+                        ->where('varname = ?', $newname);
+
+                    $result = DirectorDatafield::loadAll($this->db(), $query);
+                    if (count($result)) {
+                        throw new IcingaException('Trying to recreate "%s"',$newname);
+                    } 
+
+                    $object = DirectorDatafield::create($data, $db);
+                }
+                if (isset($datalist)) {
+                    if (!preg_match('/DataTypeDatalist$/',$object->get('datatype'))) {
+                        $object->set('datalist_id','');
+                    } else {
+                        $object->set('datalist_id', $datalist->id);
+                    }
+                }
+    
+                if (isset($target_data_type)) {
+                    if (!preg_match('/DataTypeDatalist$|DataTypeDirectorObject$|DataTypeSqlQuery$/',$object->get('datatype'))) {
+                        $object->set('data_type','');
+                    } else {
+                        $object->set('data_type', $target_data_type);
+                    }
+                }
+
+                if ($object->hasBeenModified() || $modified) {
+                    $status = $object->hasBeenLoadedFromDb() ? 200 : 201;
+                    $object->store();
+                    $response->setHttpResponseCode($status);
+                } else {
+                    $response->setHttpResponseCode(304);
+                }
+
+                return $this->sendJson($response,$this->restProps($object));
+
+ 
+            case 'DELETE':
+                $this->requireObject();
+                $this->object->delete();
+                $response->setHttpResponseCode(200);
+                $this->sendJson($response,array('message' => 'object deleted.'));
+                return;
+        }
+    }
+
+    protected function requireObject()
+    {
+        if (! $this->object) {
+            $this->getResponse()->setHttpResponseCode(404);
+            if (! $this->params->get('name')) {
+                throw new NotFoundError('You need to pass a "name" parameter to access a specific object');
+            } else {
+                throw new NotFoundError('No such object available');
+            }
+        }
+    }
+
+    protected function restProps($obj) {
+        $props=$obj->getProperties();
+        $props['object_name']=$props['varname'];
+        $props['object_type']='template';
+        foreach(array_keys($props) as $key) {
+            if (is_null($props[$key]) || in_array($key, array('id','varname'))) {
+                unset($props[$key]);
+            }
+        }
+        if ($obj->getSetting('datalist_id')) {
+            $datalist = DirectorDatalist::loadWithAutoIncId($obj->getSetting('datalist_id'),$this->db());
+            $props['datalist_name']=$datalist->list_name;
+        }
+        if ($obj->getSetting('data_type')) {
+            $props['target_data_type']=$obj->getSetting('data_type');
+        }
+        foreach (array('icinga_object_type','query','resource') as $standard_prop) {
+            if ($obj->getSetting($standard_prop)) {
+                $props[$standard_prop]=$obj->getSetting($standard_prop);
+            }
+        }
+     
+        return($props);
+    }
+
 }
--- library/Director/RestApi/IcingaObjectHandler.php
+++ library/Director/RestApi/IcingaObjectHandler.php
@@ -15,6 +15,9 @@ use Icinga\Module\Director\Objects\IcingaObject;
 use Icinga\Module\Director\Resolver\OverrideHelper;
 use InvalidArgumentException;
 use RuntimeException;
+use Icinga\Module\Director\Util;
+use Icinga\Module\Director\Web\Form\IcingaObjectFieldLoader;
+use Icinga\Module\Director\Objects\DirectorDatafield;
 
 class IcingaObjectHandler extends RequestHandler
 {
@@ -100,6 +103,11 @@ class IcingaObjectHandler extends RequestHandler
     {
         $request = $this->request;
         $db = $this->db;
+
+        if ($this->request->getActionName() === 'fields') {
+            $this->processFieldsApiRequest();
+            return;
+        }
 
         // TODO: I hate doing this:
         if ($this->request->getActionName() === 'ticket') {
@@ -190,7 +198,99 @@ class IcingaObjectHandler extends RequestHandler
             $this->persistChanges($host);
             $this->sendJson($host->toPlainObject(false, true));
         } else {
-            throw new RuntimeException('Found a single service, which should have been found (and dealt with) before');
+		throw new RuntimeException('Found a single service, which should have been found (and dealt with) before');
+	}
+    }
+
+    protected function processFieldsApiRequest() {
+        $request = $this->request;
+        $response = $this->response;
+        $db = $this->db;
+        $object = $this->requireObject();
+
+        switch ($request->getMethod()) {
+            case 'GET':
+                $r=array('objects' => array());
+                if (!$this->object->supportsFields()) {
+                    $this->sendJson($r);
+                    exit; # TODO upstream requires this to avoid not found errors
+                }
+                $loader = new IcingaObjectFieldLoader($object);
+                $fields = $loader->getFields();
+
+                foreach ($fields as $field) {
+                    $r['objects'][]=array('object_name' => $field->varname, 'object_type' => 'object', 'is_required' => $field->is_required, $this->getType().'_name' => $this->object->object_name);
+                }
+                $this->sendJson($r);
+                exit; #TODO upstream requires this to avoid not found errors
+
+            case 'PUT':
+            case 'POST':
+            case 'DELETE':
+                if (!$object->supportsFields()) {
+                    $this->getResponse()->setHttpResponseCode(400);
+                    throw new IcingaException('This object does not support fields');
+                }
+
+                $type = $this->getType();
+                $data = json_decode($request->getRawBody());
+
+                if ($data === null) {
+                    $this->getResponse()->setHttpResponseCode(400);
+                    throw new IcingaException(
+                        'Invalid JSON: %s' . $request->getRawBody(),
+                        $this->getLastJsonError()
+                    );
+                } else {
+                    $data = (array) $data;
+                }
+
+                $related_field=null;
+                if (isset($data['object_name'])) {
+                    $query = $db->getDbAdapter()
+                        ->select()
+                        ->from('director_datafield')
+                        ->where('varname = ?', $data['object_name']);
+                    $result = DirectorDatafield::loadAll($db, $query);
+                    if (count($result)) {
+                        $related_field=$result[0];
+                        $data['datafield_id']=$related_field->id;
+                    } else {
+                        throw new NotFoundError('Field does not exist: "%s"',$data['object_name']);
+                    }
+                    unset($data['object_name']);
+                } else {
+                     $this->getResponse()->setHttpResponseCode(400);
+                     throw new IcingaException('Must provide an object_name for the datafield');
+                }
+
+                unset($data['object_type']);
+                unset($data[$this->getType().'_name']);
+                $data[$type.'_id']=$this->object->id;
+               
+                $objectField = null;
+                try {
+                    $objectField = IcingaObject::loadByType($type.'Field',$data,$db);
+                    $objectField->setProperties($data);
+                } catch (Exception $e) {
+                    if ($request->getMethod() !== 'DELETE') {
+                        $objectField = IcingaObject::createByType($type.'Field',$data,$db);
+                    } else {
+                        throw $e;
+                    }
+                }
+
+                if ($request->getMethod() !== 'DELETE') {
+                    $objectField->store();
+                    $response->setHttpResponseCode(200);
+                    $this->sendJson(array('object_name' => $related_field->varname, 'object_type' => 'object', 'is_required' => $objectField->is_required));
+                    exit; #TODO upstream requires this to avoid not found errors
+                } else {
+                    $objectField->delete();
+                    $response->setHttpResponseCode(200);
+                    $this->sendJson(array('message' => 'Object Field Deleted'));
+                    exit; #TODO upstream requires this to avoid not found errors
+                } 
         }
     }
 }
