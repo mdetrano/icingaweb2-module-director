9b8d1b8a8a5227f249d08c7fbbc2db2e28b3182e
Marc DeTrano <marc@gridshield.net>
2017-02-02 16:10:53 -0600

    Implement GET API request for Datafield object

--- application/controllers/DatafieldController.php
+++ application/controllers/DatafieldController.php
@@ -3,9 +3,38 @@
 namespace Icinga\Module\Director\Controllers;
 
 use Icinga\Module\Director\Web\Controller\ActionController;
+use Icinga\Exception\InvalidPropertyException;
+use Icinga\Module\Director\Objects\DirectorDatafield;
+use Icinga\Exception\NotFoundError;
+use Icinga\Application\Hook;
+
 
 class DatafieldController extends ActionController
 {
+    protected $isApified = true;
+    protected $object;
+
+    public function init() {
+        parent::init();
+        if ($this->getRequest()->isApiRequest()) {
+            $response = $this->getResponse();
+            try {
+                $this->loadObject();
+                return $this->handleApiRequest();
+            } catch (NotFoundError $e) {
+                $response->setHttpResponseCode(404);
+                return $this->sendJson((object) array('error' => $e->getMessage()));
+            } catch (Exception $e) {
+                if ($response->getHttpResponseCode() === 200) {
+                    $response->setHttpResponseCode(500);
+                }
+
+                return $this->sendJson((object) array('error' => $e->getMessage()));
+            }
+        }
+    }
+
+
     public function addAction()
     {
         $this->indexAction();
@@ -18,6 +47,10 @@ class DatafieldController extends ActionController
 
     public function indexAction()
     {
+        if ($this->getRequest()->isApiRequest()) {
+            return;
+        }
+
         $edit = false;
 
         if ($id = $this->params->get('id')) {
@@ -43,4 +76,59 @@ class DatafieldController extends ActionController
         $form->handleRequest();
         $this->render('object/form', null, true);
     }
+
+
+    protected function loadObject()
+    {
+        if (!$this->getRequest()->getParam('name')) {
+            throw new NotFoundError('Must specify name');
+        }
+        $query = $this->db()->getDbAdapter()
+            ->select()
+            ->from('director_datafield')
+            ->where('varname = ?', $this->getRequest()->getParam('name'));
+
+        $result = DirectorDatafield::loadAll($this->db(), $query);
+        if (!count($result)) {
+            throw new NotFoundError('Got invalid name "%s"', $this->getRequest()->getParam('name'));
+        }
+        $this->object=current($result);
+        return current($result);
+    }
+
+    protected function handleApiRequest() {
+        $request = $this->getRequest();
+        $db = $this->db();
+
+        switch ($request->getMethod()) {
+            case 'GET':
+                $this->requireObject();
+                $props=$this->object->properties;
+                foreach(array_keys($props) as $key) {
+                    if (is_null($props[$key]) || $key == 'id') {
+                        unset($props[$key]);
+                    }
+                }
+                $this->sendJson($props);
+                return;
+            case 'PUT':
+            case 'POST':
+            case 'DELETE':
+                $this->sendJson(array('TODO' => 'Not yet implemented'));
+                return;
+        }
+    }
+
+    protected function requireObject()
+    {
+        if (! $this->object) {
+            $this->getResponse()->setHttpResponseCode(404);
+            if (! $this->params->get('name')) {
+                throw new NotFoundError('You need to pass a "name" parameter to access a specific object');
+            } else {
+                throw new NotFoundError('No such object available');
+            }
+        }
+    }
+
 }
